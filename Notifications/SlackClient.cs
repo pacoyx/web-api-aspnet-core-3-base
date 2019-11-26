e1f1808a4d8e7741b641c838adf2596ec1087b39
Carlos Javier Bazan Huaman <pacoyx@hotmail.com>
2019-11-26 12:23:59 -0500

    slack con hhtpcliente endok

--- Notifications/SlackClient.cs
+++ Notifications/SlackClient.cs
@@ -1,23 +1,33 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace Notifications
 {
-    public class SlackClient
+    public class SlackClient : IDisposable
     {
         private readonly Uri _uri;
         private readonly Encoding _encoding = new UTF8Encoding();
+        string urlWithAccessToken = "https://slack.com";
+        string token = "xoxp-776136247109-763416065042-776222015621-5148c2dfed24d834f2a4c87d23d805cd";
 
-        public SlackClient(string urlWithAccessToken)
+        public SlackClient()
         {
             _uri = new Uri(urlWithAccessToken);
         }
 
+        public void Dispose()
+        {
+         
+        }
+
         //Post a message using simple strings  
-        public void PostMessage(string text, string username = null, string channel = null)
+        public string PostMessage(string text, string username = null, string channel = null)
         {
             Payload payload = new Payload()
             {
@@ -26,22 +36,41 @@ namespace Notifications
                 Text = text
             };
 
-            PostMessage(payload);
+          return   PostMessage(payload);
         }
-        public void PostMessage(Payload payload)
+        public string PostMessage(Payload payload)
         {
             string payloadJson = JsonConvert.SerializeObject(payload);
+           
+           
+            using (var client = new HttpClient())
+            {            
+                client.BaseAddress = _uri;
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
 
-            using (WebClient client = new WebClient())
-            {
-                NameValueCollection data = new NameValueCollection();
-                data["payload"] = payloadJson;
+                var pairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("channel", payload.Channel),
+                    new KeyValuePair<string, string>("text", payload.Text)
+                };
 
-                var response = client.UploadValues(_uri, "POST", data);
+                var content = new FormUrlEncodedContent(pairs);
+                var response = client.PostAsync("/api/chat.postMessage", content).Result;
 
-                //The response text is usually "ok"  
-                string responseText = _encoding.GetString(response);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Mensaje enviado";
+                }
+                else
+                {
+                    return response.IsSuccessStatusCode.ToString();
+                }
             }
+
+             
+
         }
     }
 
--- apiBaseCore/Controllers/HomeController.cs
+++ apiBaseCore/Controllers/HomeController.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Notifications;
 
 namespace apiBaseCore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/home")]
     [ApiController]
     public class HomeController : ControllerBase
     {
+
+        [HttpPost , Route("testslack")]
+        public string TestSlack()
+        {          
+            
+            using (var client = new SlackClient())
+            {
+                string msj = "testiando notificacion slack";
+                string canalSlack = "#cashflow_testing";
+
+                return client.PostMessage(msj, "", canalSlack);
+            }            
+        }
+
         public string GetEmployeeName(int empId)
         {
             string name;
